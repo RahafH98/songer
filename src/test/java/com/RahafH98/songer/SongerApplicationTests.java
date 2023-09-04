9835cbb12d0e1593526c8af06fce70a20719ed71
RahafH98 <Rahafbeds99@gmail.com>
2023-09-04 19:38:37 +0300

    initial commit from Spring init

---
+++ src/test/java/com/RahafH98/songer/SongerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RahafH98.songer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
